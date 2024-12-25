497e64cc779b5457795533ff5110504fe12c3bd6
websitemake246 <hackingzone234@gmail.com>
2024-12-25 06:49:32 +0100

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  coin_balance: { type: Number, default: 0 }, // Default coins are 0
+});
+
+module.exports = mongoose.model('User', userSchema);
 
